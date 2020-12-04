f02b0e40b39039f663ac6d014d7b5d962c37ecaa
Nathan Hughes <nathan.h.hughes@gmail.com>
2020-12-04 12:17:57 -0500

    add drawing network to script to make sure they get tested

--- scripts/plot_network.py
+++ scripts/plot_network.py
@@ -10,14 +10,17 @@ def main():
     """Generate a network and draw it."""
     a = generate_network()
 
+    fig, ax = plt.subplots(2)
+    a.draw_net(ax=ax[0])
 
     with tempfile.NamedTemporaryFile() as fout:
         fout.write(a.draw_cpt_diagram().draw(format="png", prog="dot"))
         fout.seek(0)
         img_to_plot = mpimage.imread(fout.name)
-        plt.imshow(img_to_plot)
-        plt.axis("off")
-        plt.show()
+
+    ax[1].imshow(img_to_plot)
+    ax[1].axis("off")
+    plt.show()
 
 
 if __name__ == "__main__":
