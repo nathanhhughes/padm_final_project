bcd652ea8e16110d294ab0aa1c2155925861fa47
vancoykendall <coykendall.mit@gmail.com>
2020-12-06 21:50:59 -0500

    implemented bucket elimination

--- src/padm_final_project/bayes_net.py
+++ src/padm_final_project/bayes_net.py
@@ -63,12 +63,12 @@ class BayesNet:
         """
         raise NotImplementedError()
 
-    def get_MAP_estimate(self, o_nodes=[]):
+    def get_MAP_estimate(self, q_nodes, observations=dict()):
         """
         Compute the most likely configuration of the network given the evidence.
 
         Parameters:
-            o_nodes (list(Node)): List of observed nodes
+            o_nodes (dict(str->value)): dict mapping node name to its observed value
         """
         raise NotImplementedError()
 
@@ -82,19 +82,136 @@ class BayesNet:
         """
         raise NotImplementedError()
 
-    def bucket_elimination(self, o_nodes, attack_nodes):
+    def bucket_elimination(self, q_nodes, observations):
         """
-        Perform bucket elimination to solve for node ordering.
-
-        Knowing which variables are observations and which are the results
-        (attacks), perform bucket elimination on the bayes net.  This
-        simplifies the computation in the case where hidden nodes remain
-        unobservable, and observations are assumed to occur for a given set of
-        nodes.  This allows for faster subsequent computation.
+        Perform bucket elimination. 
 
         Parameters:
-            o_nodes: a list of string names for observation nodes
-            attack_nodes: a list of string names for attack nodes
+            q_nodes: a list of string names for query nodes
+            observations: a dictionary mapping node names to observations
+        """
+        # 1. (initialize buckets)
+        ordering = create_be_order(self, q_nodes, observations) # list
+        cpts = [nodes[node].probabiliteis for node in ordering]
+        buckets = dict()
+        for b in ordering: 
+            buckets[b] = []
+        # Go through each cpt function, add it to the highest ranked bucket for which the cpt includes the bucket's node
+        for cpt in reversed(cpts):
+            for node in reversed(ordering):
+                if node in cpt.columns:
+                    buckets[node].append(cpt)
+                    break
+
+
+        # 2. (Backwards)
+        for i, node in reversed(list(enumerate(ordering[1:]))):
+            i += 1
+            print('node', node, 'i', i)
+            print('ordering', ordering[:i])
+            # If an observation exists for the current bucket
+            if node in observations.keys():
+                for j, func in enumerate(buckets[node]):
+                    buckets[node].pop(j)
+                    new_func = func[func[node] == observations[node]]
+                    new_func = new_func.drop(columns=[node])
+                    new_node = get_new_node(new_func, buckets, ordering)
+                    buckets[new_node].append(new_func)
+
+            else:
+                max_or_sum = max if (node in q_nodes) else sum
+                product_func = bucket_product(buckets[node])
+                eliminated_func = eliminate_node(node, product_func, max_or_sum)
+                new_node = get_new_node(eliminated_func, buckets, ordering)
+                buckets[new_node].append(eliminated_func)
+                if max_or_sum == max:
+                    argmax_func = eliminate_node(node, product_func, np.argmax)
+                    argmax_func.rename(columns={'prob': node}, inplace=True)
+                    argmax_func[node] = argmax_func[node].apply(lambda x: x==1)
+                    buckets[node] = [argmax_func]
+
+        final_node = ordering[0]
+        final_product = bucket_product(buckets[final_node])
+        buckets[final_node] = [final_product]
+
+        return buckets
+
+    @staticmethod
+    def bucket_product(funcs):
+    """
+    Creates a single function that is the product of multiple functions.
+    Used by bucket elimination to get the product of all the functions in a bucket.
+    """
+    # Get set of nodes involved in all the functions
+    nodes = set()
+    for func in funcs: 
+        nodes = nodes.union(set(func.columns))
+    nodes.remove('prob')
+    nodes = list(nodes)
+    
+    # Create all permutations of node assignments
+    node_values = [True, False] # Assumes binary nodes (must implement different logic to get node_values if nodes aren't binary)
+    perms = set()
+    for c in itertools.combinations_with_replacement(node_values, len(nodes)):
+        for p in itertools.permutations(c):
+            perms.add(p)
+            
+    # Convert func dfs to dfs with a multiindex corresponding to node assignments
+    # Only column left is 'prob' column
+    mi_funcs = []
+    for func in funcs:
+        midx = pd.MultiIndex.from_frame(func[func.columns[:-1]])
+        mi_funcs.append(func[['prob']].set_index(midx))
+        
+    # Compute the probability for each assignment and create new df
+    rows = []
+    for perm in perms:
+        assignment = dict(zip(nodes, perm))
+        product = 1
+        for mi_func in mi_funcs:
+            key = tuple(assignment[node] for node in mi_func.index.names)
+            product *= mi_func.loc[key]['prob']
+        rows.append(list(perm)+[product])
+    return pd.DataFrame(rows, columns=nodes+['prob'])
+
+    @staticmethod
+    def eliminate_node(node, func, elim_func):
+    
+    """
+    Takes a function and a node to sum out of the function.
+    Used by bucket elimination at each bucket after func_product() to sum out the bucket's node.
+    """
+    node_values = [True, False]
+    nodes = list(func.columns)
+    nodes.remove(node)
+    nodes.remove('prob')
+    
+    # Get all permutations of node assignmnets not being summed out
+    perms = []
+    for perm in func[nodes].drop_duplicates().iterrows():
+        perms.append(tuple(perm[1]))
+    print('perms', perms)
+    midx = pd.MultiIndex.from_frame(func[nodes])
+    mi_func = func[[node, 'prob']].set_index(midx)
+    
+    rows = []
+    for perm in perms:
+        probability = elim_func(mi_func.loc[perm].prob)
+        rows.append(list(perm)+[probability])
+    return pd.DataFrame(rows, columns=nodes+['prob'])
+
+    @staticmethod
+    def get_new_node(func, buckets, ordering):
+        """
+        Helper function for bucket elimination. Returns the bucket in which to place the provided function.
+        """
+        for node in reversed(ordering):
+            if node in func.columns:
+                return node
+            
+    def get_be_ordering(self, q_nodes, observations):
+        """
+        Computes the ordering for the bucket elimination method
         """
         raise NotImplementedError()
 
