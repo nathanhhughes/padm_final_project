6fe699159b813ef77eec9a6bf6b509d3e1cfc967
Nathan Hughes <nathan.h.hughes@gmail.com>
2020-12-09 10:25:42 -0500

    move utilities from notebook to make example cleaner

---
+++ src/padm_final_project/display_utils.py
@@ -0,0 +1,32 @@
+"""Module containing some quick helper functions for pretty printing an example."""
+
+
+def display_state(network, observations, map_estimates=None, add_newline=True):
+    """Show observed values of the nodes of a network."""
+    to_return = "" if not add_newline else "\n"
+
+    if not map_estimates:
+        map_estimates = []
+    to_return += "**Observations:**\n"
+
+    for node in network.nodes.values():
+        node_text = node.name if node.label is None else node.label
+        node_value = "?" if node.name not in observations else observations[node.name]
+        if node.name in map_estimates:
+            node_value = "{} (est)".format(observations[node.name])
+        to_return += " - *{}* = {}\n".format(node_text, node_value)
+
+    return to_return
+
+
+def display_posteriors(variables, P_variables, add_newline=True):
+    """Show probabilities for variables."""
+    to_return = "" if not add_newline else "\n"
+    to_return += "**Posteriors:**\n"
+
+    for varname, prob_table in zip(variables, P_variables):
+        prob = prob_table.iloc[1]["prob"]
+        to_return += r"- $P(\text{{{}}} = T) = {:1.4f}$".format(varname, prob)
+        to_return += "\n"
+
+    return to_return
